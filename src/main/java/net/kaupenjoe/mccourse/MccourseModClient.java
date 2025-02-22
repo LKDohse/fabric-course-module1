45da2a1c48d4401b65df50f54c4719d9b9baaf64
ElectricBudgie <lkdohse@gmail.com>
2025-02-21 20:58:26 -0500

    Initial commit

---
+++ src/main/java/net/kaupenjoe/mccourse/MccourseModClient.java
@@ -0,0 +1,8 @@
+package net.kaupenjoe.mccourse;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MccourseModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient(){}
+}
