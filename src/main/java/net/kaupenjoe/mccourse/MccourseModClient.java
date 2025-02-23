6f0274d23e027408e931234c84ffd7e206314831
ElectricBudgie <lkdohse@gmail.com>
2025-02-23 12:08:26 -0500

    End of module 1

--- src/main/java/net/kaupenjoe/mccourse/MccourseModClient.java
+++ src/main/java/net/kaupenjoe/mccourse/MccourseModClient.java
@@ -1,8 +1,14 @@
 package net.kaupenjoe.mccourse;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.kaupenjoe.mccourse.block.ModBlocks;
+import net.minecraft.client.render.RenderLayer;
 
 public class MccourseModClient implements ClientModInitializer {
     @Override
-    public void onInitializeClient(){}
+    public void onInitializeClient(){
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.FLUORITE_DOOR, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.FLUORITE_TRAPDOOR, RenderLayer.getCutout());
+    }
 }
