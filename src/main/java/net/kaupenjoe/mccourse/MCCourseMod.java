1a9258d7865e176cce4b7385f3d8a1b1a60132a6
ElectricBudgie <lkdohse@gmail.com>
2025-03-30 13:29:56 -0400

    added home commands

--- src/main/java/net/kaupenjoe/mccourse/MCCourseMod.java
+++ src/main/java/net/kaupenjoe/mccourse/MCCourseMod.java
@@ -2,12 +2,17 @@ package net.kaupenjoe.mccourse;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
 import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.kaupenjoe.mccourse.block.ModBlocks;
+import net.kaupenjoe.mccourse.command.ReturnHomeCommand;
+import net.kaupenjoe.mccourse.command.SetHomeCommand;
 import net.kaupenjoe.mccourse.components.ModDataComponentTypes;
 import net.kaupenjoe.mccourse.event.AttackEntityHandler;
+import net.kaupenjoe.mccourse.event.PlayerCopyHandler;
 import net.kaupenjoe.mccourse.item.ModItemGroups;
 import net.kaupenjoe.mccourse.item.ModItems;
 import net.kaupenjoe.mccourse.util.HammerUsageEvent;
@@ -32,5 +37,9 @@ public class MCCourseMod implements ModInitializer {
 
         //PlayerBlockBreakEvents.BEFORE.register(new HammerUsageEvent()); //this is causing a bug where you can't break any blocks
         AttackEntityCallback.EVENT.register(new AttackEntityHandler());
+
+        CommandRegistrationCallback.EVENT.register(ReturnHomeCommand::register);
+        CommandRegistrationCallback.EVENT.register(SetHomeCommand::register);
+        ServerPlayerEvents.COPY_FROM.register(new PlayerCopyHandler());
     }
 }
---
+++ src/main/java/net/kaupenjoe/mccourse/event/PlayerCopyHandler.java
@@ -0,0 +1,14 @@
+package net.kaupenjoe.mccourse.event;
+
+import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
+import net.kaupenjoe.mccourse.util.IEntityDataSaver;
+import net.minecraft.server.network.ServerPlayerEntity;
+
+public class PlayerCopyHandler implements ServerPlayerEvents.CopyFrom{
+    @Override
+    public void copyFromPlayer(ServerPlayerEntity oldPlayer, ServerPlayerEntity newPlayer, boolean isAlive) {
+        ((IEntityDataSaver) newPlayer).getPersistentData().putIntArray("mccourse.homepos",
+                ((IEntityDataSaver)oldPlayer).getPersistentData().getIntArray("mccourse.homepos"));
+
+    }
+}
