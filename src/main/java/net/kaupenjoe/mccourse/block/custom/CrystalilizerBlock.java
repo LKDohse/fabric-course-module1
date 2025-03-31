d8f65a5c617c919163e5c86c3a73e33fb5cdf961
ElectricBudgie <lkdohse@gmail.com>
2025-03-31 00:02:35 -0400

    add particles for Crystallizer

--- src/main/java/net/kaupenjoe/mccourse/block/custom/CrystalilizerBlock.java
+++ src/main/java/net/kaupenjoe/mccourse/block/custom/CrystalilizerBlock.java
@@ -3,9 +3,15 @@ package net.kaupenjoe.mccourse.block.custom;
 import com.mojang.serialization.MapCodec;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.particle.BlockStateParticleEffect;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.state.StateManager;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 public class CrystalilizerBlock extends HorizontalFacingBlock {
@@ -29,5 +35,29 @@ public class CrystalilizerBlock extends HorizontalFacingBlock {
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
         builder.add(FACING);
+    }
+
+    @Override
+    public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
+        //CLIENT ONLY
+    double xPos = pos.getX() + 0.5f;
+    double yPos = pos.getY() + 1.25f;
+    double zPos = pos.getZ() + 0.5f;
+    double offset = random.nextDouble() * 0.6 - 0.3;
+
+    world.addParticle(ParticleTypes.SMOKE, xPos + offset, yPos, zPos + offset, 0.0, 0.0, 0.0);
+    world.addParticle(ParticleTypes.GLOW, xPos + offset, yPos, zPos + offset, 0.0, 0.0, 0.0);
+
+    /*
+    other examples
+     using items as particles
+     world.addParticle(new ItemStackParticleEffect(ParticleTypes.ITEM, Items.COOKED_BEEF.getDefaultStack()),
+     xPos + offset, yPos, zPos + offset, 0.0, 0.0, 0.0);
+    using blocks as particles
+            world.addParticle(new BlockStateParticleEffect(ParticleTypes.BLOCK,
+                    Blocks.DIAMOND_BLOCK.getDefaultState()), xPos + offset, yPos, zPos + offset, 0.0, 0.0, 0.0 );
+    */
+
+
     }
 }
