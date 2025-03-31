46d4f537efcfb59dbd18f09a76cda7c0446b6b83
ElectricBudgie <lkdohse@gmail.com>
2025-03-30 21:34:40 -0400

    add custom villager trades

--- src/main/java/net/kaupenjoe/mccourse/MCCourseMod.java
+++ src/main/java/net/kaupenjoe/mccourse/MCCourseMod.java
@@ -5,6 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
 import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
+import net.fabricmc.fabric.api.object.builder.v1.trade.TradeOfferHelper;
 import net.fabricmc.fabric.api.registry.FabricBrewingRecipeRegistryBuilder;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.kaupenjoe.mccourse.block.ModBlocks;
@@ -19,8 +20,12 @@ import net.kaupenjoe.mccourse.item.ModItems;
 import net.kaupenjoe.mccourse.potion.ModPotions;
 import net.kaupenjoe.mccourse.sound.ModSounds;
 import net.minecraft.block.ComposterBlock;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.potion.Potions;
+import net.minecraft.village.TradeOffer;
+import net.minecraft.village.TradedItem;
+import net.minecraft.village.VillagerProfession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -46,11 +51,32 @@ public class MCCourseMod implements ModInitializer {
             builder.registerPotionRecipe(Potions.AWKWARD, Items.SLIME_BALL, ModPotions.SLIMY_POTION);
         });
 
+        registerCustomTrades();
+
         //PlayerBlockBreakEvents.BEFORE.register(new HammerUsageEvent()); //this is causing a bug where you can't break any blocks
         AttackEntityCallback.EVENT.register(new AttackEntityHandler());
 
         CommandRegistrationCallback.EVENT.register(ReturnHomeCommand::register);
         CommandRegistrationCallback.EVENT.register(SetHomeCommand::register);
         ServerPlayerEvents.COPY_FROM.register(new PlayerCopyHandler());
+    }
+
+    public static void registerCustomTrades() {
+        TradeOfferHelper.registerVillagerOffers(VillagerProfession.FARMER, 1, factories -> {
+            factories.add(((entity, random) -> new TradeOffer(
+                    new TradedItem(Items.EMERALD, 2),
+                    new ItemStack(ModItems.STRAWBERRY, 6), 6, 2, 0.04f
+            )));
+            factories.add(((entity, random) -> new TradeOffer(
+                    new TradedItem(Items.EMERALD, 9),
+                    new ItemStack(ModItems.CHAINSAW, 1), 1, 6, 0.09f
+            )));
+        });
+        TradeOfferHelper.registerVillagerOffers(VillagerProfession.TOOLSMITH, 2, factories -> {
+            factories.add(((entity, random) -> new TradeOffer(
+                    new TradedItem(Items.DIAMOND, 6),
+                    new ItemStack(ModItems.FLUORITE, 19), 4, 1, 0.04f
+            )));
+        });
     }
 }
